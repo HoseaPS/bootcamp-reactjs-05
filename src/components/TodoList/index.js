2717678ba133512afdd3ee059b5979cdb9552fc6
Oséias P. Silva <hoseaps@gmail.com>
2019-02-13 20:50:51 -0200

    testing user click and using TDD

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,17 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class TodoList extends Component {
   state = {
     todos: []
   };
 
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "Novo todo" }]
+    });
+  };
+
+  removeTodo = id => {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    });
+  };
+
   render() {
     return (
-      <ul>
-        {this.state.todos.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
+      <Fragment>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li onClick={() => this.removeTodo(todo.id)} key={todo.id}>
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+        <button onClick={this.addTodo}>Adicionar todo</button>
+      </Fragment>
     );
   }
 }
